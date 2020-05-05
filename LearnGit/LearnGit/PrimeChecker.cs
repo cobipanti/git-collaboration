3079c74725e260b2f8c0ac36dd7cfc550570f641
Cobi Panti <cobi.panti@gmail.com>
2020-05-04 16:15:45 -1000

    Add Prime Checker class. Make changes to Start Menu in incorporate the new class.

---
+++ LearnGit/LearnGit/PrimeChecker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnGit
+{
+    class PrimeChecker
+    {
+
+        public PrimeChecker()
+        {
+            Console.WriteLine("Please enter a number to check if its a prime number or not.");
+
+            while (true)
+            {
+
+                string pInput = Console.ReadLine();
+
+                int num;
+                bool canConvert = int.TryParse(pInput, out num);
+
+
+                if (canConvert)
+                {
+                    bool isPrime = true;
+                    for (int i = 2; i <= Math.Sqrt(num); i++)
+                    {
+                        if (num % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if (isPrime)
+                    {
+                        Console.WriteLine($"{num} is a prime number.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num} is NOT a prime number.\n");
+                    }
+                }
+
+                else
+                {
+                    string pOrder = pInput.ToLower();
+                    if(pOrder == "q" || pOrder == "quit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("invalid input.\n");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LearnGit/LearnGit/StartMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnGit
+{
+    class StartMenu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            new StartMenu();
+        }
+
+        public StartMenu()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine();
+                string order = input.ToLower();
+
+                if (order == "h" || order == "help")
+                {
+                    Console.WriteLine("help\n");
+                }
+                else if (order == "q" || order == "quit")
+                {
+                    break;
+                }
+                else if (order == "p" || order == "prime")
+                {
+                    new PrimeChecker();
+                    Console.WriteLine("Welcome back to the Main Menu.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please type in 'h' or 'help' for help");
+                }
+            }
+        }
+    }
+}
